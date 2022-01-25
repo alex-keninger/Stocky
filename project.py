c310d2cbfb7029e4c031fa05b991aa580f0617d7
akeninger7 <76502103+akeninger7@users.noreply.github.com>
2022-01-24 22:49:20 -0600

    Update project.py

--- project.py
+++ project.py
@@ -1,10 +1,38 @@
 import streamlit as st
+from datetime import date
 import pandas as pd
+
+
 
 st.write("""
 # My first app
 # Hello *Alex!*
 # """)
+
+import yfinance as yf
+#from fbprophet import Prophet
+#from fbprophet.plot import plot_plotly
+#from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediction App")
+
+stocks = ("AAPL", "GOOG", "MSFT", "GME")
+selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 1, 4)
+period = n_years * 365
+
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stocks)
+data_load_state.text("Loading data...done!")
 
 
 
