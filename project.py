da1f5e937e4a98a3d12cdccb06d0c3e100d35d87
akeninger7 <76502103+akeninger7@users.noreply.github.com>
2022-02-16 17:54:14 -0600

    2/16 Update
    -Added the ability for user to input stock themselves
    ---Included an error catcher to disallow tickers that do not exist
    -Added the ability for a user to select the range of time they want to see stock data

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+from pyparsing import null_debug_action
+from sqlalchemy import null
 import streamlit as st #imports streamlit and changes its reference to st
 from datetime import date
 import pandas as pd # imports panda library and changes its reference to pd
@@ -7,6 +9,7 @@ import pandas as pd # imports panda library and changes its reference to pd
 st.write("""
 # CS 460 Stock Analysis Web App
 # Hello *Bozo!*
+##### "Enter a description of the app here"
 # """)
 ###
 import yfinance as yf
@@ -17,18 +20,15 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("Stock Prediction App")
+st.title("Stock Analyzer")
 
 st.sidebar.write("Example sidebar text") #st.sidebar puts widgets and texts in a sidebar on your page
 
 
-
-
-stocks = ("APRE","AAPL", "GOOG", "MSFT", "GME")
-selected_stocks = st.selectbox("Select dataset for prediction", stocks)
-
-#n_years = st.slider("Years of prediction:", 1, 4)
-#period = n_years * 365
+#stocks = ("APRE","AAPL", "GOOG", "MSFT", "GME")
+selected_stocks = st.text_input("Input stock")
+if selected_stocks == "": #displays info box when no ticker is entered
+    st.info("Enter your ticker in the box above")
 
 @st.cache #caches the data, doesnt need to run this code again if stock has been cached
 def load_data(ticker):
@@ -36,16 +36,43 @@ def load_data(ticker):
     data.reset_index(inplace=True)
     return data
 
+
 data_load_state = st.text("Load data...")
-data = load_data(selected_stocks)
+if selected_stocks == "":
+    data = load_data("AAPL")
+    st.warning("Currently showing data for AAPL. Try inputing your own ticker!")
+else:
+    data = load_data(selected_stocks)
+
+while True: #Catchs invalid tickers and produces an error message
+    try: 
+        testingbox = data.at[0,"Date"] #checks to see if there is any "date" data
+        break
+    except KeyError: 
+        st.error("Please enter a valid ticker")
+        break
+
 data_load_state.text("Loading data...done!")
+
+st.write()
+DateRange = st.selectbox("How far would you like to look back?", ["1 week", "1 month", "3 months", "1 year", "All Time"])
+if DateRange == "1 week":
+    DateRange = 7
+elif DateRange == "1 month":
+    DateRange = 31
+elif DateRange == "3 months":
+    DateRange = 93
+elif DateRange == "1 year":
+    DateRange = 365
 
 
 st.subheader('Raw data')
-st.write(data.tail()) 
+if DateRange == "All Time":
+    st.write(data)
+else:
+    st.write(data.tail(DateRange)) 
     #creates a table with data entries from yahoo finance
-    # tail cuts off the last entries (default of 5)
-    #this is also already a pandas dataframe so stremlit can handle it
+    # tail cuts off the last entries determined by the DateRange
 
 def plot_raw_data(): #configures and creates plot.ly graph, could also look into matplotlib graph
     fig = go.Figure()
