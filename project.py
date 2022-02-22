20df8e1955c563a5a139ba05796482a8a213f2d2
akeninger7 <76502103+akeninger7@users.noreply.github.com>
2022-02-21 20:50:27 -0600

    2/21 Update
    -Added Navigation functionally to create different pages
    -Began work on sidebar watchlist

--- project.py
+++ project.py
@@ -1,85 +1,97 @@
-from pyparsing import null_debug_action
-from sqlalchemy import null
+from tkinter import W
 import streamlit as st #imports streamlit and changes its reference to st
 from datetime import date
 import pandas as pd # imports panda library and changes its reference to pd
-
-
-
-st.write("""
-# CS 460 Stock Analysis Web App
-# Hello *Bozo!*
-##### "Enter a description of the app here"
-# """)
-###
 import yfinance as yf
-#from prophet import Prophet
-#from prophet.plot import plot_plotly
+    #from prophet import Prophet
+    #from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
+watchlist_stocks = []
 
-st.title("Stock Analyzer")
+page_names = ["Main Page", "Page 2"]
+page = st.sidebar.radio('Navigation', page_names)
 
-st.sidebar.write("Example sidebar text") #st.sidebar puts widgets and texts in a sidebar on your page
+if page == "Main Page":
+    st.write("""
+    # CS 460 Stock Analysis Web App
+    # Hello!
+    ##### "Enter a description of the app here"
+    # """)
+    
+    st.title("Stock Analyzer")
 
+    #stocks = ("APRE","AAPL", "GOOG", "MSFT", "GME")
+    selected_stocks = st.text_input("Input stock")
+    if selected_stocks == "": #displays info box when no ticker is entered
+        st.info("Enter your ticker in the box above")
 
-#stocks = ("APRE","AAPL", "GOOG", "MSFT", "GME")
-selected_stocks = st.text_input("Input stock")
-if selected_stocks == "": #displays info box when no ticker is entered
-    st.info("Enter your ticker in the box above")
-
-@st.cache #caches the data, doesnt need to run this code again if stock has been cached
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
-
-
-data_load_state = st.text("Load data...")
-if selected_stocks == "":
-    data = load_data("AAPL")
-    st.warning("Currently showing data for AAPL. Try inputing your own ticker!")
-else:
-    data = load_data(selected_stocks)
-
-while True: #Catchs invalid tickers and produces an error message
-    try: 
-        testingbox = data.at[0,"Date"] #checks to see if there is any "date" data
-        break
-    except KeyError: 
-        st.error("Please enter a valid ticker")
-        break
-
-data_load_state.text("Loading data...done!")
-
-st.write()
-DateRange = st.selectbox("How far would you like to look back?", ["1 week", "1 month", "3 months", "1 year", "All Time"])
-if DateRange == "1 week":
-    DateRange = 7
-elif DateRange == "1 month":
-    DateRange = 31
-elif DateRange == "3 months":
-    DateRange = 93
-elif DateRange == "1 year":
-    DateRange = 365
+    @st.cache #caches the data, doesnt need to run this code again if stock has been cached
+    def load_data(ticker):
+        data = yf.download(ticker, START, TODAY)
+        data.reset_index(inplace=True)
+        return data
 
 
-st.subheader('Raw data')
-if DateRange == "All Time":
-    st.write(data)
-else:
-    with st.expander("Click to expand"):
-        st.write(data.tail(DateRange)) 
-    #creates a table with data entries from yahoo finance
-    # tail cuts off the last entries determined by the DateRange
+    data_load_state = st.text("Load data...")
+    if selected_stocks == "":
+        data = load_data("AAPL")
+        st.warning("Currently showing data for AAPL. Try inputing your own ticker!")
+    else:
+        data = load_data(selected_stocks)
 
-def plot_raw_data(): #configures and creates plot.ly graph, could also look into matplotlib graph
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x= data['Date'], y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x= data['Date'], y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig) #creates the plotly graph integrated with streamlit
+    while True: #Catchs invalid tickers and produces an error message
+        try: 
+            testingbox = data.at[0,"Date"] #checks to see if there is any "date" data
+            break
+        except KeyError: 
+            st.error("Please enter a valid ticker")
+            break
 
-plot_raw_data()
+    data_load_state.text("Loading data...done!")
+
+    st.write()
+    DateRange = st.selectbox("How far would you like to look back?", ["1 week", "1 month", "3 months", "1 year", "All Time"])
+    if DateRange == "1 week":
+        DateRange = 7
+    elif DateRange == "1 month":
+        DateRange = 31
+    elif DateRange == "3 months":
+        DateRange = 93
+    elif DateRange == "1 year":
+        DateRange = 365
+
+
+    st.subheader('Raw data')
+    if DateRange == "All Time":
+        st.write(data)
+    else:
+        with st.expander("Click to expand"):
+            st.write(data.tail(DateRange)) 
+        #creates a table with data entries from yahoo finance
+        # tail cuts off the last entries determined by the DateRange
+
+    def plot_raw_data(): #configures and creates plot.ly graph, could also look into matplotlib graph
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x= data['Date'], y=data['Open'], name='stock_open'))
+        fig.add_trace(go.Scatter(x= data['Date'], y=data['Close'], name='stock_close'))
+        fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig) #creates the plotly graph integrated with streamlit
+
+    plot_raw_data()
+
+elif page == "Page 2":
+    st.button("Click Me")
+
+
+st.sidebar.text("")
+st.sidebar.text("Watchlist (Work in Progress)") #st.sidebar puts widgets and texts in a sidebar on your page
+
+watchstock = st.sidebar.text_input("Enter a stock to add to watchlist")
+watchlist_stocks.append(watchstock)
+st.sidebar.text(watchlist_stocks)
+selected_watch_stock = st.sidebar.selectbox("View a stock", watchlist_stocks)
+
+
